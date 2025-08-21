8ea6112f4eaf1780c247faf64eb4c902e57927ea
Astitva Arya <147731572+Joyboy48@users.noreply.github.com>
2025-08-21 12:52:56 +0530

    Update DownLoadResumeBtn.tsx

--- src/components/DownLoadResumeBtn.tsx
+++ src/components/DownLoadResumeBtn.tsx
@@ -1,20 +1,14 @@
-import { cn } from '@/lib/utils'
-import { Download } from 'lucide-react'
-import Link from 'next/link'
 import React from 'react'
-import { buttonVariants } from './ui/button'
 import HackerBtn from './animation/HackerBtn'
 
 function DownLoadResumeBtn() {
   return (
     <div className="h-fit w-full mt-2 py-2 px-4">
-      <a href="/astitva-resume01.pdf" target="_blank" rel="noopener noreferrer" download>
+      <a href="/astitva-resume01.pdf" target="_blank" rel="noopener noreferrer">
         <HackerBtn label='Download Resume' />
       </a>
     </div>
   )
 }
 
-
 export default DownLoadResumeBtn
-
