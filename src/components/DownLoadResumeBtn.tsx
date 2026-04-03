d6a61c88f638113a71aa8e847644137839223b17
Astitva Arya <147731572+Joyboy48@users.noreply.github.com>
2026-04-03 14:15:40 +0530

    Update DownLoadResumeBtn.tsx

--- src/components/DownLoadResumeBtn.tsx
+++ src/components/DownLoadResumeBtn.tsx
@@ -8,7 +8,7 @@ import HackerBtn from './animation/HackerBtn'
 function DownLoadResumeBtn() {
   return (
     <div className="h-fit w-full mt-2 py-2 px-4">
-      <a href="/my_resume001.pdf" target="_blank" rel="noopener noreferrer" download>
+      <a href="/new resume 4.pdf" target="_blank" rel="noopener noreferrer" download>
         <HackerBtn label='Download Resume' />
       </a>
     </div>
